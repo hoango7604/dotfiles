92a50f7eb78fd0655ca96955c5afcf29adb3d761
Apple <apple@Mac.local>
2021-02-06 00:54:43 +0700

    fix restore file to match config directory

--- restore.sh
+++ restore.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 # Clone dotfiles in to workspace
-mkdir -p $HOME/workspace
 git clone https://github.com/hoango7604/dotfiles.git $HOME/dotfiles
 cd $HOME/dotfiles
 
 # Restore runtime config
 while read f; do
   echo "Copying file $f..."
-  cp -rf $f ~/
+  cp -rf ./config/$f ~/
 done < Dotfiles
 
 echo "Restore dotfiles done!"
